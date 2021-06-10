bcbebb14710fa55c43625660ece2c1c8cd2d290e
Josh <josh.mcfayden@gmail.com>
2021-06-10 11:10:47 +0200

    Changing python shebang and making credentials path configurable

--- calendar-scraper.py
+++ calendar-scraper.py
@@ -1,4 +1,4 @@
-B0;95;0c#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # includes for gCal API
 from __future__ import print_function
@@ -10,7 +10,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-
+cred_dir = os.environ["HOME"]
 # includes for ics API
 from ics import Calendar
 import requests
@@ -118,7 +118,7 @@ def main():
             for event in events_to_write:
                 insert_gcal_event(service,out_ID,event)
                 
-    print(f'Found a total of {total_new_events} new event(s) to write.')
+    print(f'\nFound a total of {total_new_events} new event(s) to write.')
     
 def get_gcalsvc():
     """Gets google calendar API service"""
@@ -127,18 +127,18 @@ def get_gcalsvc():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists(cred_dir+'/token.json'):
+        creds = Credentials.from_authorized_user_file(cred_dir+'/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                cred_dir+'/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(cred_dir+'/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
